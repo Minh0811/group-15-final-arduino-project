27d9d9081df0a7adabc932f3ac1a56225ae6e49a
sangnguyen0403 <102713198+sangnguyen0403@users.noreply.github.com>
2022-08-29 14:43:53 +0700

    update openlid function

--- ultraSonicAverage.cpp
+++ ultraSonicAverage.cpp
@@ -2,11 +2,13 @@
 #include <Servo.h>  //servo motor
 #include <Adafruit_MLX90614.h>  //temperature sensor
 
+Servo myservo;
 // HC-SR04 (UltraSonic)
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 3 // attach pin D3 Arduino to pin Trig of HC-SR04
 long duration;    // variable for the duration of sound wave travel
 int distance;     // variable for the distance measurement
+int pos = 0;
 #define SOUND_VELOCITY 0.034
 
 void setup()
@@ -15,6 +17,7 @@ void setup()
     // HC-SR04
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
     pinMode(echoPin, INPUT);  // Sets the echoPin as an INPUT
+    myservo.attach(10); // access input pin #10
 }
 
 void ultraSonic()
@@ -30,8 +33,18 @@ void ultraSonic()
     distance = duration * SOUND_VELOCITY / 2; // Calculate the distance
 }
 
+// Openlid done
+// weight: wait for ESP2866 to be done
+
 void loop()
 {
-    ultraSonic();
-    delay(200);
+  ultraSonic_openlid(); // 2 ultrasonic sensors: openlid + weight
+  if (distance <= 5) {
+    myservo.write(90);
+    delay(15000);
+  } else {
+    myservo.write(0);
+    // delay(15);
+  }
+    delay(500);
 }
