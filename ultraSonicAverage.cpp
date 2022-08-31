0cbdf9b156dafe754f45ecde591c8e8257683c25
Quynh (May) Tran <68426318+quynhtran216@users.noreply.github.com>
2022-08-31 15:33:24 +0700

    add in water sensor

--- ultraSonicAverage.cpp
+++ ultraSonicAverage.cpp
@@ -9,6 +9,7 @@ long duration;    // variable for the duration of sound wave travel
 int distance;     // variable for the distance measurement
 int pos = 0;
 #define SOUND_VELOCITY 0.034
+#define Grove_Water_Sensor 8 // attach water sensor to Arduino Digital Pin 8
 
 void setup()
 {
@@ -17,9 +18,10 @@ void setup()
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
     pinMode(echoPin, INPUT);  // Sets the echoPin as an INPUT
     myservo.attach(10); // access input pin #10
+    pinMode(Grove_Water_Sensor, INPUT);
 }
 
-void ultraSonic()
+void ultraSonic_openlid()
 {
     digitalWrite(trigPin, LOW); // Clears the trigPin
     delayMicroseconds(2);
@@ -30,17 +32,26 @@ void ultraSonic()
     duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
 
     distance = duration * SOUND_VELOCITY / 2; // Calculate the distance
+    Serial.println(distance);
 }
 
 // Openlid done
 // weight: wait for ESP2866 to be done
+
+// Water leaking function
+void waterSensor()
+{
+  if (digitalRead(Grove_Water_Sensor) == LOW) {
+    //Insert code to send notification back to the user
+  }
+}
 
 void loop()
 {
   ultraSonic_openlid(); // 2 ultrasonic sensors: openlid + weight
   if (distance <= 5) {
     myservo.write(90);
-    delay(15000);
+    delay(10000);
   } else {
     myservo.write(0);
     // delay(15);
