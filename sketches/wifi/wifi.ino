09b5791392ba5227c22f76c94591a84596a7ddc2
minh0811 <vokhaiminh0811@gmail.com>
2022-09-06 18:11:58 +0700

    add code for wifi and tripwire but unfinished

--- sketches/wifi/wifi.ino
+++ sketches/wifi/wifi.ino
@@ -1,38 +1,8 @@
-// https://www.youtube.com/watch?v=6-RXqFS_UtU
-
-// https://www.youtube.com/watch?v=YLKEZtLhfZo
-// https://blynk.io/
-
-
-//wifi
-
-// https://www.youtube.com/watch?v=2cjufbgOBYo&list=PL9UWJqkOPUWQj76q1ycopIgc4SwHzlGQ6&index=4&t=185s
-// code
-/*
-   -- New project --
-
-   This source code of graphical user interface
-   has been generated automatically by RemoteXY editor.
-   To compile this code using RemoteXY library 3.1.8 or later version
-   download by link http://remotexy.com/en/library/
-   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/
-     - for ANDROID 4.11.1 or later version;
-     - for iOS 1.9.1 or later version;
-
-   This source code is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-*/
-
-//////////////////////////////////////////////
-//        RemoteXY include library          //
-//////////////////////////////////////////////
-
-// RemoteXY select connection mode and include library
 #define REMOTEXY_MODE__ESP8266_HARDSERIAL_POINT
 
 #include <RemoteXY.h>
+#include <Wire.h>
+#include <BH1750.h>
 
 // RemoteXY connection settings
 #define REMOTEXY_SERIAL Serial
@@ -41,22 +11,21 @@
 #define REMOTEXY_WIFI_PASSWORD "12345678"
 #define REMOTEXY_SERVER_PORT 6377
 
-// RemoteXY configurate
+// RemoteXY configurate  
 #pragma pack(push, 1)
-uint8_t RemoteXY_CONF[] = // 36 bytes
-    {255, 2, 0, 0, 0, 29, 0, 16, 31, 1, 2, 0, 35, 58, 22, 11, 2, 26, 31, 31,
-     79, 78, 0, 79, 70, 70, 0, 1, 0, 13, 51, 12, 12, 2, 31, 0};
-
-// this structure defines all the variables and events of your control interface
-struct
-{
+uint8_t RemoteXY_CONF[] =   // 60 bytes
+  { 255,1,0,0,0,53,0,16,67,1,2,1,17,43,27,13,37,26,31,31,
+  79,78,0,79,70,70,0,129,0,4,19,56,5,94,80,114,101,115,115,32,
+  116,111,32,100,105,115,97,98,108,101,32,116,114,105,112,119,105,114,101,0 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
 
     // input variables
-    uint8_t switch_1; // =1 if switch ON and =0 if OFF
-    uint8_t button_1; // =1 if button pressed, else =0
+  uint8_t switch_1; // =1 if switch ON and =0 if OFF 
 
     // other variable
-    uint8_t connect_flag; // =1 if wire connected, else =0
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
 
 } RemoteXY;
 #pragma pack(pop)
@@ -65,27 +34,67 @@ struct
 //           END RemoteXY include          //
 /////////////////////////////////////////////
 
-#define PIN_SWITCH_1 13
-#define PIN_BUTTON_1 12
+#define PIN_SWITCH_1 7
 
-void setup()
+// light sensor => BH1750)
+#define ADDR 0b0100011
+BH1750 GY30;
+float lux;
+
+// buzzer => KY-012
+int buzzerPin = 8;
+void setup() 
 {
-    RemoteXY_Init();
+  RemoteXY_Init (); 
+  
+  pinMode (PIN_SWITCH_1, OUTPUT);
+  
+  // TODO you setup code
+      // light sensor
+    Serial.begin(115200);                             // launch the serial monitor
+    Wire.begin();                                   // Initialize the I2C bus for use by the BH1750 library
+    GY30.begin(BH1750::CONTINUOUS_HIGH_RES_MODE_2); // Initialize the sensor object
+    Serial.println("Flux Workshop Example");
 
-    pinMode(PIN_SWITCH_1, OUTPUT);
-    pinMode(PIN_BUTTON_1, OUTPUT);
-
-    // TODO you setup code
+    // buzzer
+    pinMode(buzzerPin, OUTPUT);
 }
 
-void loop()
+void loop() 
+{ 
+  RemoteXY_Handler ();
+  
+  digitalWrite(PIN_SWITCH_1, (RemoteXY.switch_1==0)?LOW:HIGH);
+  
+  // TODO you loop code
+  // use the RemoteXY structure for data transfer
+  // do not call delay() 
+    //lightSensor();
+    //buzzer();
+    lux = GY30.readLightLevel();     
+//    if (lux > 2000){
+//      digitalWrite(buzzerPin, HIGH);
+//      }
+//     else{
+//      digitalWrite(buzzerPin, LOW);
+//      }
+}
+void lightSensor()
 {
-    RemoteXY_Handler();
-
-    digitalWrite(PIN_SWITCH_1, (RemoteXY.switch_1 == 0) ? LOW : HIGH);
-    digitalWrite(PIN_BUTTON_1, (RemoteXY.button_1 == 0) ? LOW : HIGH);
-
-    // TODO you loop code
-    // use the RemoteXY structure for data transfer
-    // do not call delay()
+    lux = GY30.readLightLevel();                      // read the light level from the sensor and store it in a variable
+    Serial.println((String) "Light: " + lux + " lx"); // print the data to the serial monitor
+    //delay(100);                                       // Pause for a second before repeating the sensor poll
+}
+void buzzer()
+{
+    if (lux > 500000)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            digitalWrite(buzzerPin, HIGH);
+            delay(500);
+            digitalWrite(buzzerPin, LOW);
+            delay(500);
+        }
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,58 @@
+#define REMOTEXY_MODE__ESP8266_HARDSERIAL_POINT
+
+#include <RemoteXY.h>
 #include <Wire.h>
 #include <BH1750.h>
 
-BH1750 GY30;
+// RemoteXY connection settings
+#define REMOTEXY_SERIAL Serial
+#define REMOTEXY_SERIAL_SPEED 115200
+#define REMOTEXY_WIFI_SSID "uktf"
+#define REMOTEXY_WIFI_PASSWORD "12345678"
+#define REMOTEXY_SERVER_PORT 6377
 
-// laser => KY-008
-int laserPin = 12;
+// RemoteXY configurate
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] = // 60 bytes
+    {255, 1, 0, 0, 0, 53, 0, 16, 67, 1, 2, 1, 17, 43, 27, 13, 37, 26, 31, 31,
+     79, 78, 0, 79, 70, 70, 0, 129, 0, 4, 19, 56, 5, 94, 80, 114, 101, 115, 115, 32,
+     116, 111, 32, 100, 105, 115, 97, 98, 108, 101, 32, 116, 114, 105, 112, 119, 105, 114, 101, 0};
+
+// this structure defines all the variables and events of your control interface
+struct
+{
+
+    // input variables
+    uint8_t switch_1; // =1 if switch ON and =0 if OFF
+
+    // other variable
+    uint8_t connect_flag; // =1 if wire connected, else =0
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+#define PIN_SWITCH_1 7
 
 // light sensor => BH1750)
 #define ADDR 0b0100011
+BH1750 GY30;
 float lux;
+
 // buzzer => KY-012
 int buzzerPin = 8;
-
 void setup()
 {
-    // laser
-    pinMode(laserPin, OUTPUT);
+    RemoteXY_Init();
 
+    pinMode(PIN_SWITCH_1, OUTPUT);
+
+    // TODO you setup code
     // light sensor
-    Serial.begin(9600);                             // launch the serial monitor
+    Serial.begin(115200);                           // launch the serial monitor
     Wire.begin();                                   // Initialize the I2C bus for use by the BH1750 library
     GY30.begin(BH1750::CONTINUOUS_HIGH_RES_MODE_2); // Initialize the sensor object
     Serial.println("Flux Workshop Example");
@@ -29,25 +63,32 @@ void setup()
 
 void loop()
 {
-    laser();
-    lightSensor();
-    buzzer();
-}
+    RemoteXY_Handler();
 
-void laser()
-{
-    digitalWrite(laserPin, HIGH);
-}
+    digitalWrite(PIN_SWITCH_1, (RemoteXY.switch_1 == 0) ? LOW : HIGH);
 
+    // TODO you loop code
+    // use the RemoteXY structure for data transfer
+    // do not call delay()
+    // lightSensor();
+    // buzzer();
+    lux = GY30.readLightLevel();
+    //    if (lux > 2000){
+    //      digitalWrite(buzzerPin, HIGH);
+    //      }
+    //     else{
+    //      digitalWrite(buzzerPin, LOW);
+    //      }
+}
 void lightSensor()
 {
     lux = GY30.readLightLevel();                      // read the light level from the sensor and store it in a variable
     Serial.println((String) "Light: " + lux + " lx"); // print the data to the serial monitor
-    delay(100);                                       // Pause for a second before repeating the sensor poll
+    // delay(100);                                       // Pause for a second before repeating the sensor poll
 }
 void buzzer()
 {
-    if (lux < 50)
+    if (lux > 500000)
     {
         for (int i = 0; i < 10; i++)
         {
